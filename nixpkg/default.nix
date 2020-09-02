2013e61a04de7b5756b55547e3b422536def8754
Ben Burdette <bburdette@gmail.com>
2020-09-02 08:40:45 -0600

    nixpkg

---
+++ nixpkg/default.nix
@@ -0,0 +1,55 @@
+{ stdenv
+, fetchFromGitHub
+, rustPlatform
+, openssl
+, pkgconfig
+, sqlite
+, callPackage }:
+
+# , lib
+# , packr
+
+rustPlatform.buildRustPackage rec {
+  pname = "zknotes-server";
+  version = "1.0";
+
+  # ui = callPackage ./ui.nix { };
+
+  src = fetchFromGitHub {
+    owner = "bburdette";
+    repo = "zknotes";
+    rev = "06a33d172f22ebdbdde6064f9d7c028061e2377a";
+    sha256 = "0v9b6mar6cf0cqdwc2q8snm2lfqkblniql3sbqv74r36swqb30wy";
+  };
+
+  # preBuild = ''
+  #   cp -r ${ui}/libexec/gotify-ui/deps/gotify-ui/build ui/build && packr
+  # '';
+
+  # postInstall = ''
+  #   echo "postInttall"
+  #   ls -l $out
+  #   cp -r ${ui}/static $out
+  # '';
+
+  # cargo-culting this from the gotify package.
+  subPackages = [ "." ];
+
+
+  sourceRoot = "source/server";
+  cargoSha256 = "0b1m6lpqms8i14j9rvpbx92shhrxzj0wnkqvjssykcn4wp0hfvwb";
+  # dontMakeSourcesWritable=1;
+
+  buildInputs = [openssl sqlite];
+
+  nativeBuildInputs = [ pkgconfig ];
+
+  meta = with stdenv.lib; {
+    description = "zknotes zettelkasten server.";
+    homepage = https://github.com/bburdette/zknotes;
+    license = with licenses; [ gpl ];
+    maintainers = [ ];
+    platforms = platforms.all;
+  };
+}
+
---
+++ nixpkg/test.sh
@@ -0,0 +1,1 @@
+~/code/nix-error-project/nix/inst/bin/nix-build -E --show-trace 'with import <nixpkgs> { }; callPackage ./default.nix {}'
