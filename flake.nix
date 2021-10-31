632ccf5de381d8df87292c16ab093eae1aa67a33
Ben Burdette <bburdette@gmail.com>
2021-10-31 15:05:33 -0600

    point at nixpkgs 21.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   description = "zknotes, a web based zettelkasten";
 
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/21.05;
+
   outputs = { self, nixpkgs }: {
 
     packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
