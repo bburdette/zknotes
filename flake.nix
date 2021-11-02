f30a069e90be318c2134359b1b4541b9e030eca3
Ben Burdette <bburdette@gmail.com>
2021-11-02 09:05:54 -0600

    convert direnv stuff to use flake.nix

--- flake.nix
+++ flake.nix
@@ -81,7 +81,17 @@
               pkgconfig
               openssl.dev 
               elmPackages.elm
+              elmPackages.elm-analyse
+              elmPackages.elm-doc-preview
+              elmPackages.elm-format
               elmPackages.elm-live
+              elmPackages.elm-test
+              elmPackages.elm-upgrade
+              elmPackages.elm-xref
+              # nixos2009.elmPackages.elm-language-server
+              elmPackages.elm-language-server
+              elmPackages.elm-verify-examples
+              elmPackages.elmi-to-json
               elmPackages.elm-optimize-level-2
             ];
           };
