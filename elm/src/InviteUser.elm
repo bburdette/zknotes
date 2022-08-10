9c11b57cd33a1ca5f66801dc6f64c3600859437d
Ben Burdette <bburdette@gmail.com>
2022-08-10 12:03:29 -0600

    adding search and recent panels

--- elm/src/InviteUser.elm
+++ elm/src/InviteUser.elm
@@ -1,6 +1,7 @@
 module InviteUser exposing (GDModel, Model, Msg(..), init, update, view)
 
-import Data exposing (zklKey)
+import Common
+import Data exposing (Direction(..), zklKey)
 import Dict exposing (Dict(..))
 import Element as E exposing (Element)
 import Element.Background as EBk
@@ -15,14 +16,36 @@ import SearchPanel as SP
 import TangoColors as TC
 import Time exposing (Zone)
 import Util
+import ZkCommon as ZC
+
+
+linkButtonStyle =
+    Common.buttonStyle
+
+
+disabledLinkButtonStyle =
+    Common.disabledButtonStyle
+
+
+type NavChoice
+    = NcSearch
+    | NcRecent
+
+
+type SearchOrRecent
+    = SearchView
+    | RecentView
 
 
 type alias Model =
-    { loginData : OD.LoginData
+    { loginData : Data.LoginData
     , email : String
     , spmodel : SP.Model
     , zklDict : Dict String Data.EditLink
     , zknSearchResult : Data.ZkListNoteSearchResult
+    , searchOrRecent : SearchOrRecent
+    , focusSr : Maybe Int -- note id in search result.
+    , focusLink : Maybe Data.EditLink
     }
 
 
@@ -30,6 +53,17 @@ type Msg
     = EmailChanged String
     | OkClick
     | CancelClick
+    | SearchHistoryPress
+    | AddToSearch Data.ZkListNote
+    | AddToSearchAsTag String
+    | ToLinkPress Data.ZkListNote
+    | FromLinkPress Data.ZkListNote
+    | SrFocusPress Int
+    | LinkFocusPress Data.EditLink
+    | FlipLink Data.EditLink
+    | RemoveLink Data.EditLink
+    | SPMsg SP.Msg
+    | NavChoiceChanged NavChoice
     | Noop
 
 
@@ -37,9 +71,9 @@ type alias GDModel =
     GD.Model Model Msg OD.GetInvite
 
 
-init : SP.Model -> Data.ZkListNoteSearchResult -> List Data.EditLink -> OD.LoginData -> List (E.Attribute Msg) -> Element () -> GDModel
-init spmodel spresult links loginData buttonStyle underLay =
-    { view = view buttonStyle
+init : SP.Model -> Data.ZkListNoteSearchResult -> List Data.ZkListNote -> List Data.EditLink -> Data.LoginData -> List (E.Attribute Msg) -> Element () -> GDModel
+init spmodel spresult recentZkns links loginData buttonStyle underLay =
+    { view = view buttonStyle recentZkns
     , update = update
     , model =
         { loginData = loginData
@@ -48,13 +82,353 @@ init spmodel spresult links loginData buttonStyle underLay =
         , zklDict =
             Dict.fromList (List.map (\zl -> ( zklKey zl, zl )) links)
         , zknSearchResult = spresult
+        , searchOrRecent = SearchView
+        , focusSr = Nothing
+        , focusLink = Nothing
         }
     , underLay = underLay
     }
 
 
-view : List (E.Attribute Msg) -> Maybe Util.Size -> Model -> Element Msg
-view buttonStyle mbsize model =
+showSr : Model -> Data.ZkListNote -> Element Msg
+showSr model zkln =
+    let
+        lnnonme =
+            zkln.user /= model.loginData.userid
+
+        sysColor =
+            ZC.systemColor model.loginData zkln.sysids
+
+        controlrow =
+            E.row [ E.spacing 8, E.width E.fill ]
+                [ (case
+                    Dict.get (zklKey { direction = To, otherid = zkln.id })
+                        model.zklDict
+                   of
+                    Just _ ->
+                        Nothing
+
+                    Nothing ->
+                        Just 1
+                  )
+                    |> Maybe.map
+                        (\_ ->
+                            EI.button linkButtonStyle
+                                { onPress = Just <| ToLinkPress zkln
+                                , label = E.el [ E.centerY ] <| E.text "→"
+                                }
+                        )
+                    |> Maybe.withDefault
+                        (EI.button
+                            disabledLinkButtonStyle
+                            { onPress = Nothing
+                            , label = E.el [ E.centerY ] <| E.text "→"
+                            }
+                        )
+                , (case
+                    Dict.get (zklKey { direction = From, otherid = zkln.id })
+                        model.zklDict
+                   of
+                    Just _ ->
+                        Nothing
+
+                    Nothing ->
+                        Just 1
+                  )
+                    |> Maybe.map
+                        (\_ ->
+                            EI.button linkButtonStyle
+                                { onPress = Just <| FromLinkPress zkln
+                                , label = E.el [ E.centerY ] <| E.text "←"
+                                }
+                        )
+                    |> Maybe.withDefault
+                        (EI.button
+                            disabledLinkButtonStyle
+                            { onPress = Nothing
+                            , label = E.el [ E.centerY ] <| E.text "←"
+                            }
+                        )
+                , EI.button linkButtonStyle
+                    { onPress = Just (AddToSearch zkln)
+                    , label = E.text "^"
+                    }
+                , EI.button linkButtonStyle
+                    { onPress = Just (AddToSearchAsTag zkln.title)
+                    , label = E.text "t"
+                    }
+                , if lnnonme then
+                    E.link
+                        ZC.otherLinkStyle
+                        { url = Data.editNoteLink zkln.id
+                        , label = E.text zkln.title
+                        }
+
+                  else
+                    E.link
+                        ZC.myLinkStyle
+                        { url = Data.editNoteLink zkln.id
+                        , label = E.text "go"
+                        }
+                ]
+
+        listingrow =
+            E.el
+                ([ E.width E.fill
+                 , EE.onClick (SrFocusPress zkln.id)
+                 , E.height <| E.px 30
+                 , E.clipX
+                 ]
+                    ++ (sysColor
+                            |> Maybe.map (\c -> [ EF.color c ])
+                            |> Maybe.withDefault []
+                       )
+                )
+            <|
+                E.text zkln.title
+    in
+    if model.focusSr == Just zkln.id then
+        -- focus result!  show controlrow.
+        E.column
+            [ EBd.width 1
+            , E.padding 3
+            , EBd.rounded 3
+            , EBd.color TC.darkGrey
+            , E.width E.fill
+            ]
+            [ listingrow, controlrow ]
+
+    else
+        listingrow
+
+
+showZkl : Maybe Data.EditLink -> Data.LoginData -> Maybe Int -> Maybe E.Color -> Bool -> Data.EditLink -> Element Msg
+showZkl focusLink ld id sysColor showflip zkl =
+    let
+        ( dir, otherid ) =
+            case zkl.direction of
+                To ->
+                    ( E.text "→", Just zkl.otherid )
+
+                From ->
+                    ( E.text "←", Just zkl.otherid )
+
+        focus =
+            focusLink
+                |> Maybe.map
+                    (\l ->
+                        zkl.direction
+                            == l.direction
+                            && zkl.otherid
+                            == l.otherid
+                            && zkl.user
+                            == l.user
+                    )
+                |> Maybe.withDefault False
+
+        display =
+            [ E.el
+                [ E.height <| E.px 30
+                ]
+                dir
+            , zkl.othername
+                |> Maybe.withDefault ""
+                |> (\s ->
+                        E.el
+                            ([ E.clipX
+                             , E.height <| E.px 30
+                             , E.width E.fill
+                             , EE.onClick (LinkFocusPress zkl)
+                             ]
+                                ++ (sysColor
+                                        |> Maybe.map (\c -> [ EF.color c ])
+                                        |> Maybe.withDefault []
+                                   )
+                            )
+                            (E.text s)
+                   )
+            ]
+    in
+    if focus then
+        E.column
+            [ E.spacing 8
+            , E.width E.fill
+            , EBd.width 1
+            , E.padding 3
+            , EBd.rounded 3
+            , EBd.color TC.darkGrey
+            ]
+            [ E.row [ E.spacing 8, E.width E.fill ] display
+            , E.row [ E.spacing 8 ]
+                [ if ld.userid == zkl.user then
+                    EI.button (linkButtonStyle ++ [ E.alignLeft ])
+                        { onPress = Just (RemoveLink zkl)
+                        , label = E.text "X"
+                        }
+
+                  else
+                    EI.button (linkButtonStyle ++ [ E.alignLeft, EBk.color TC.darkGray ])
+                        { onPress = Nothing
+                        , label = E.text "X"
+                        }
+                , case zkl.othername of
+                    Just name ->
+                        EI.button (linkButtonStyle ++ [ E.alignLeft ])
+                            { onPress = Just <| AddToSearchAsTag name
+                            , label = E.text ">"
+                            }
+
+                    Nothing ->
+                        E.none
+                , if showflip then
+                    EI.button (linkButtonStyle ++ [ E.alignLeft ])
+                        { onPress = Just (FlipLink zkl)
+                        , label = E.text "⇄"
+                        }
+
+                  else
+                    E.none
+                ]
+            ]
+
+    else
+        E.row [ E.spacing 8, E.width E.fill, E.height <| E.px 30 ] display
+
+
+view : List (E.Attribute Msg) -> List Data.ZkListNote -> Maybe Util.Size -> Model -> Element Msg
+view buttonStyle recentZkns mbsize model =
+    let
+        sppad =
+            [ E.paddingXY 0 5 ]
+
+        spwidth =
+            E.px
+                400
+
+        showLinks =
+            E.row [ EF.bold ] [ E.text "links" ]
+                :: List.map
+                    (\( l, c ) ->
+                        showZkl
+                            model.focusLink
+                            model.loginData
+                            Nothing
+                            c
+                            (Dict.get
+                                (zklKey
+                                    { otherid = l.otherid
+                                    , direction =
+                                        case l.direction of
+                                            To ->
+                                                From
+
+                                            From ->
+                                                To
+                                    }
+                                )
+                                model.zklDict
+                                |> Util.isJust
+                                |> not
+                            )
+                            l
+                    )
+                    (Dict.values model.zklDict
+                        |> List.map
+                            (\l ->
+                                ( l
+                                , ZC.systemColor model.loginData l.sysids
+                                )
+                            )
+                        |> List.sortWith
+                            (\( l, lc ) ( r, rc ) ->
+                                case ( lc, rc ) of
+                                    ( Nothing, Nothing ) ->
+                                        compare r.otherid l.otherid
+
+                                    ( Just _, Nothing ) ->
+                                        GT
+
+                                    ( Nothing, Just _ ) ->
+                                        LT
+
+                                    ( Just lcolor, Just rcolor ) ->
+                                        case Util.compareColor lcolor rcolor of
+                                            EQ ->
+                                                compare r.otherid l.otherid
+
+                                            a ->
+                                                a
+                            )
+                    )
+
+        searchPanel =
+            E.column
+                (E.spacing 8 :: E.width E.fill :: sppad)
+                (E.row [ E.width E.fill ]
+                    [ EI.button Common.buttonStyle
+                        { onPress = Just <| SearchHistoryPress
+                        , label = E.el [ E.centerY ] <| E.text "search history"
+                        }
+                    ]
+                    :: (E.map SPMsg <|
+                            SP.view True True 0 model.spmodel
+                       )
+                    :: (List.map
+                            (showSr model)
+                        <|
+                            model.zknSearchResult.notes
+                       )
+                    ++ (if List.length model.zknSearchResult.notes < 15 then
+                            []
+
+                        else
+                            [ E.map SPMsg <|
+                                SP.paginationView True model.spmodel
+                            ]
+                       )
+                )
+
+        recentPanel =
+            E.column (E.spacing 8 :: sppad)
+                (List.map
+                    (showSr model)
+                 <|
+                    recentZkns
+                )
+
+        searchOrRecentPanel =
+            E.column
+                [ E.spacing 8
+                , E.alignTop
+                , E.alignRight
+                , E.width spwidth
+                , EBd.width 1
+                , EBd.color TC.darkGrey
+                , EBd.rounded 10
+                , EBk.color TC.white
+                , E.clip
+                ]
+                (Common.navbar 2
+                    (case model.searchOrRecent of
+                        SearchView ->
+                            NcSearch
+
+                        RecentView ->
+                            NcRecent
+                    )
+                    NavChoiceChanged
+                    [ ( NcSearch, "search" )
+                    , ( NcRecent, "recent" )
+                    ]
+                    :: [ case model.searchOrRecent of
+                            SearchView ->
+                                searchPanel
+
+                            RecentView ->
+                                recentPanel
+                       ]
+                )
+    in
     E.column
         [ E.width (mbsize |> Maybe.map .width |> Maybe.withDefault 500 |> E.px)
         , E.height E.shrink
@@ -66,6 +440,7 @@ view buttonStyle mbsize model =
             , placeholder = Nothing
             , label = EI.labelLeft [] (E.text "email")
             }
+        , searchOrRecentPanel
         , E.row [ E.width E.fill, E.spacing 10 ]
             [ EI.button buttonStyle
                 { onPress = Just OkClick, label = E.text "Ok" }
@@ -92,4 +467,37 @@ update msg model =
                 }
 
         Noop ->
+            GD.Dialog model
+
+        SearchHistoryPress ->
+            GD.Dialog model
+
+        AddToSearch zkListNote ->
+            GD.Dialog model
+
+        AddToSearchAsTag string ->
+            GD.Dialog model
+
+        ToLinkPress zkListNote ->
+            GD.Dialog model
+
+        FromLinkPress zkListNote ->
+            GD.Dialog model
+
+        SrFocusPress int ->
+            GD.Dialog model
+
+        LinkFocusPress editLink ->
+            GD.Dialog model
+
+        FlipLink editLink ->
+            GD.Dialog model
+
+        RemoveLink editLink ->
+            GD.Dialog model
+
+        SPMsg sPMsg ->
+            GD.Dialog model
+
+        NavChoiceChanged navChoice ->
             GD.Dialog model
