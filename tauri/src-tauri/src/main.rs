05731df98e284f3806616f8daaa4e78531a069c9
Ben Burdette <>
2023-05-15 11:53:16 -0600

    mobile changes

--- tauri/src-tauri/src/main.rs
+++ tauri/src-tauri/src/main.rs
@@ -3,12 +3,22 @@
   windows_subsystem = "windows"
 )]
 
-fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+pub fn main() {
+  app::AppBuilder::new().run();
 }
+
+// #[cfg(mobile)]
+// pub fn main() {
+//   app::AppBuilder::new().run();
+// }
+
+// #[cfg(desktop)]
+// fn main() {
+//   tauri::Builder::default()
+//     .invoke_handler(tauri::generate_handler![greet])
+//     .run(tauri::generate_context!())
+//     .expect("error while running tauri application");
+// }
 
 #[tauri::command]
 fn greet(name: &str) -> String {
