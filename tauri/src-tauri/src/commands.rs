49f9d8639cd6369d63266782083461f7e38ed148
Ben Burdette <>
2023-10-18 11:05:22 -0600

    creating db file

--- tauri/src-tauri/src/commands.rs
+++ tauri/src-tauri/src/commands.rs
@@ -1,4 +1,5 @@
 use serde_json::Value;
+use std::sync::Mutex;
 use tauri::State;
 use zknotes_server_lib::err_main;
 use zknotes_server_lib::orgauth::data::WhatMessage;
@@ -6,8 +7,8 @@ use zknotes_server_lib::orgauth::endpoints::{Callbacks, Tokener, UuidTokener};
 use zknotes_server_lib::zkprotocol::messages::{PublicMessage, ServerResponse, UserMessage};
 
 pub struct ZkState {
-  pub config: zknotes_server_lib::config::Config,
-  pub uid: Option<i64>,
+  pub config: Mutex<zknotes_server_lib::config::Config>,
+  pub uid: Mutex<Option<i64>>,
 }
 
 #[tauri::command]
@@ -27,7 +28,11 @@ pub fn zimsg(state: State<ZkState>, msg: UserMessage) -> ServerResponse {
   //   Some(uid) =>
   // }
 
-  match zknotes_server_lib::interfaces::zk_interface_loggedin(&&state.config, 2, &msg) {
+  match zknotes_server_lib::interfaces::zk_interface_loggedin(
+    &&state.config.lock().unwrap(),
+    2,
+    &msg,
+  ) {
     Ok(sr) => {
       println!("sr: {}", sr.what);
       // serde_json::to_value(&sr).unwrap());
@@ -47,7 +52,7 @@ pub fn pimsg(state: State<ZkState>, msg: PublicMessage) -> ServerResponse {
 
   println!("pimsg");
 
-  match zknotes_server_lib::interfaces::public_interface(&state.config, msg, None) {
+  match zknotes_server_lib::interfaces::public_interface(&state.config.lock().unwrap(), msg, None) {
     Ok(sr) => {
       println!("sr: {}", sr.what);
       // serde_json::to_value(&sr).unwrap());
@@ -69,7 +74,11 @@ pub fn uimsg(state: State<ZkState>, msg: WhatMessage) -> WhatMessage {
 
   let mut ut = UuidTokener { uuid: None };
 
-  let sr = match zknotes_server_lib::interfaces::user_interface(&mut ut, &state.config, msg) {
+  let sr = match zknotes_server_lib::interfaces::user_interface(
+    &mut ut,
+    &state.config.lock().unwrap(),
+    msg,
+  ) {
     Ok(sr) => {
       println!("sr: {}", sr.what);
       // serde_json::to_value(&sr).unwrap());
--- tauri/src-tauri/src/lib.rs
+++ tauri/src-tauri/src/lib.rs
@@ -1,21 +1,31 @@
 mod commands;
 use commands::{greet, pimsg, uimsg, zimsg, ZkState};
-use zknotes_server_lib::sqldata;
+use std::sync::Mutex;
+use tauri::Manager;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-  let config = zknotes_server_lib::defcon();
-  let ret = zknotes_server_lib::sqldata::dbinit(
-    config.orgauth_config.db.as_path(),
-    config.orgauth_config.login_token_expiration_ms,
-  );
-
-  println!("dbinit ret: {:?}", ret);
-
   tauri::Builder::default()
     .manage(ZkState {
-      config: zknotes_server_lib::defcon(),
-      uid: None,
+      config: Mutex::new(zknotes_server_lib::defcon()),
+      uid: Mutex::new(None),
+    })
+    .setup(|app| {
+      let mut dbpath = app.path().data_dir().unwrap();
+      dbpath.push("zknotes.db");
+      println!("dbpath: {:?}", dbpath);
+      match app.state::<ZkState>().config.lock() {
+        Ok(mut config) => {
+          config.orgauth_config.db = dbpath;
+          zknotes_server_lib::sqldata::dbinit(
+            config.orgauth_config.db.as_path(),
+            config.orgauth_config.login_token_expiration_ms,
+          );
+        }
+        Err(_) => (),
+      }
+
+      Ok(())
     })
     .invoke_handler(tauri::generate_handler![greet, zimsg, pimsg, uimsg])
     .run(tauri::generate_context!())
