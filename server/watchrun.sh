127c3fc8678ec58df3bb85e5cff6871a5f8dc649
Ben Burdette <>
2023-11-02 12:19:49 -0600

    minor config tweaks

--- server/watchrun.sh
+++ server/watchrun.sh
@@ -1,1 +1,1 @@
-cargo watch  -c -w src -w ../orgauth/rustlib/src/ -w ../zkprotocol/src/ -w Cargo.toml -w ../zkprotocol/Cargo.toml -w ../orgauth/rustlib/src/ -w ../orgauth/rustlib/Cargo.toml -x build -s ./run-tauri.sh
+cargo watch  -c -w src -w ../orgauth/rustlib/src/ -w ../zkprotocol/src/ -w Cargo.toml -w ../zkprotocol/Cargo.toml -w ../orgauth/rustlib/src/ -w ../orgauth/rustlib/Cargo.toml -x build -s ./run.sh
