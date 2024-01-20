bb36b32487e9be9a300b070cd9a484c31f1b1fba
Ben Burdette <>
2024-01-19 22:58:35 -0700

    SyncMessage

--- zkprotocol/src/content.rs
+++ zkprotocol/src/content.rs
@@ -1,6 +1,6 @@
 #![allow(non_snake_case)]
 
-use crate::search::ZkListNoteSearchResult;
+use crate::search::{ZkListNoteSearchResult, ZkSearchResultHeader};
 use uuid::Uuid;
 
 pub type ZkNoteId = Uuid;
@@ -198,7 +198,7 @@ pub struct GetZkNoteComments {
   pub limit: Option<i64>,
 }
 
-#[derive(Serialize, Debug, Clone)]
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct ZkNoteAndLinks {
   pub zknote: ZkNote,
   pub links: Vec<EditLink>,
@@ -208,4 +208,79 @@ pub struct ZkNoteAndLinks {
 pub struct ZkNoteAndLinksWhat {
   pub what: String,
   pub znl: ZkNoteAndLinks,
+}
+
+// Represents a remote user that is not registered on this server.
+#[derive(Clone, Deserialize, Serialize, Debug)]
+pub struct ZkPhantomUser {
+  pub id: i64,
+  pub uuid: Uuid,
+  pub name: String,
+  pub active: bool,
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+pub enum SyncMessage {
+  PhantomUser(ZkPhantomUser),
+  ZkSearchResultHeader(ZkSearchResultHeader),
+  Uuid(Uuid),
+  ZkListNote(ZkListNote),
+  ZkNote(ZkNote),
+  ZkNoteAndLinks(ZkNoteAndLinks),
+  ArchiveZkLinkHeader,
+  ArchiveZkLink(ArchiveZkLink),
+  UuidZkLinkHeader,
+  UuidZkLink(UuidZkLink),
+}
+
+impl From<ZkPhantomUser> for SyncMessage {
+  fn from(a: ZkPhantomUser) -> Self {
+    SyncMessage::PhantomUser(a)
+  }
+}
+
+impl From<ZkSearchResultHeader> for SyncMessage {
+  fn from(a: ZkSearchResultHeader) -> Self {
+    SyncMessage::ZkSearchResultHeader(a)
+  }
+}
+impl From<Uuid> for SyncMessage {
+  fn from(a: Uuid) -> Self {
+    SyncMessage::Uuid(a)
+  }
+}
+impl From<ZkListNote> for SyncMessage {
+  fn from(a: ZkListNote) -> Self {
+    SyncMessage::ZkListNote(a)
+  }
+}
+impl From<ZkNote> for SyncMessage {
+  fn from(a: ZkNote) -> Self {
+    SyncMessage::ZkNote(a)
+  }
+}
+impl From<ZkNoteAndLinks> for SyncMessage {
+  fn from(a: ZkNoteAndLinks) -> Self {
+    SyncMessage::ZkNoteAndLinks(a)
+  }
+}
+// impl From<ArchiveZkLinkHeader> for SyncMessage {
+//   fn from(a: ArchiveZkLinkHeader) -> Self {
+//     SyncMessage::ArchiveZkLinkHeader(a)
+//   }
+// }
+impl From<ArchiveZkLink> for SyncMessage {
+  fn from(a: ArchiveZkLink) -> Self {
+    SyncMessage::ArchiveZkLink(a)
+  }
+}
+// impl From<UuidZkLinkHeader> for SyncMessage {
+//   fn from(a: UuidZkLinkHeader) -> Self {
+//     SyncMessage::UuidZkLinkHeader(a)
+//   }
+// }
+impl From<UuidZkLink> for SyncMessage {
+  fn from(a: UuidZkLink) -> Self {
+    SyncMessage::UuidZkLink(a)
+  }
 }
