7b298e9a721247d4a0fa7b54aa9a3c3ada95c557
Ben Burdette <>
2024-06-21 12:25:28 -0600

    try toml settings

--- module.nix
+++ module.nix
@@ -6,6 +6,7 @@ let
 
   cfg = config.services.zknotes;
   opt = options.services.zknotes;
+  settingsFormat = pkgs.formats.toml { };
 
 in
 
@@ -43,16 +44,48 @@ in
       #   description = "Listen on a specific IP address.";
       # };
 
-      # listenPort = mkOption {
-      #   type = types.nullOr types.int;
-      #   default = null;
-      #   example = 8011;
-      #   description = "Listen on a specific IP port.";
-      # };
+    settings = lib.mkOption {
+      inherit (settingsFormat) type;
+      default = ''
+        ip = '127.0.0.1'
+        port = 8000
+        createdirs = false
+        altmainsite = []
+        file_tmp_path = './temp'
+        file_path = './files'
+
+        [orgauth_config]
+        mainsite = 'http://localhost:8000'
+        appname = 'zknotes'
+        emaildomain = 'zknotes.com'
+        db = './zknotes.db'
+        admin_email = 'admin@admin.admin'
+        regen_login_tokens = true
+        email_token_expiration_ms = 86400000
+        reset_token_expiration_ms = 86400000
+        invite_token_expiration_ms = 604800000
+        open_registration = false
+        send_emails = false
+        non_admin_invite = true
+        remote_registration = true '';
+      description = ''
+        Configuration included in `starship.toml`.
+
+        See https://starship.rs/config/#prompt for documentation.
+      '';
+    };
+
+      listenPort = mkOption {
+        type = types.nullOr types.int;
+        default = null;
+        example = 8011;
+        description = "Listen on a specific IP port.";
+      };
 
     };
   };
 
+          # ${pkgs.zknotes}/bin/zknotes-server --write_config config.toml
   ###### implementation
   config = mkIf cfg.enable {
 
@@ -71,8 +104,8 @@ in
         if [ ! -f config.toml ]; then
           mkdir -p files
           mkdir -p temp
-          ${pkgs.zknotes}/bin/zknotes-server --write_config config.toml
         fi
+        echo ${settings} >> config.toml
         RUST_LOG=info ${pkgs.zknotes}/bin/zknotes-server -c config.toml
         '';
     };
