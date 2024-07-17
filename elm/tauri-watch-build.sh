f52c2d97c2649e278741a173a19646dbbe77dccf
Ben Burdette <>
2024-07-17 15:19:47 -0600

    output to tauri static

--- elm/tauri-watch-build.sh
+++ elm/tauri-watch-build.sh
@@ -1,2 +1,3 @@
-elm-live src/Main.elm -d ../../tauri/src-tauri/static -h 0.0.0.0 --port 8010 -- --output=../server/static/main.js 
+elm-live src/Main.elm -d ../../tauri/src-tauri/static -h 0.0.0.0 --port 8010 -- --output=../../tauri/src-tauri/static/main.js
+# elm-live src/Main.elm -d ../../tauri/src-tauri/static -h 0.0.0.0 --port 8010 -- --output=../server/static/main.js
 # elm-live src/Main.elm -d ../server/static -h 10.0.0.223 --port 8010 -- --output=../server/static/main.js --debug
