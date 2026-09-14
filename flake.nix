92e13a5152560e997f0ef68982ba941525b0788e
Ben Burdette <ben@interstitial.coop>
2026-09-14 10:37:07 -0600

    nix flake update

--- flake.nix
+++ flake.nix
@@ -107,15 +107,15 @@
               openssl.dev
               elm2nix
               elmPackages.elm
-              elmPackages.elm-analyse
-              elmPackages.elm-doc-preview
+              # elmPackages.elm-analyse
+              # elmPackages.elm-doc-preview
               elmPackages.elm-format
               elmPackages.elm-live
               elmPackages.elm-test
               elmPackages.elm-upgrade
-              elmPackages.elm-xref
+              # elmPackages.elm-xref
               elmPackages.elm-language-server
-              elmPackages.elm-verify-examples
+              # elmPackages.elm-verify-examples
               elmPackages.elm-optimize-level-2
             ];
 
